275b101c87bcc3b1200d5308f4776e95ad6f5f13
dragonprevost <dragon@dkp.io>
2019-03-25 21:11:19 -0700

    basic formation done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+  	super(props);
+	this.state = {
+	  url: '',
+	}
+  }
+
+  handleTextChange = (e) => {
+  	this.setState({
+	  [e.target.id]: e.target.value,
+	});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+			{ this.state.source &&
+					<ReactPlayer 
+					  url={ this.state.source } 
+					  controls={ true }
+					/>
+			}
+			<label>Video URL</label>
+			<input id='source' onChange={ this.handleTextChange } />
+		</header>
       </div>
     );
   }
